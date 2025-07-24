f09212856437e94110ef73cb4d5f8f1b294d225b
Salitha Kaweendra <salitha.wijerathna@gmail.com>
2025-07-24 13:13:37 +0530

    CHG | add ecosystem configuration for frontend application

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: 'frontend',
+    script: 'npm run start',
+    args: 'start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+}
